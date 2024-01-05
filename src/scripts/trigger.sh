183b550dd33c8fb5a0d9ea06bc79419f66f7769d
Justin Dalton <justin@mutinyhq.com>
2024-01-05 17:35:00 -0500

    initial circleback trigger command

---
+++ src/scripts/trigger.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# This example uses envsubst to support variable substitution in the string parameter type.
+# https://circleci.com/docs/orbs-best-practices/#accepting-parameters-as-strings-or-environment-variables
+
+CIRCLE_TOKEN=${!PARAM_CIRCLECI_API_KEY}
+PARAMETERS="{\"circleback-orb-pipeline-id\":\"$PARAM_PIPELINE_ID\"}"
+
+# merge PARAM_PARAMETERS into PARAMETERS if it exists
+if [ -n "$PARAM_PARAMETERS" ]; then
+  PARAMETERS=$(jq -n --argjson json1 "$PARAMETERS" --argjson json2 "$PARAM_PARAMETERS" '$json1 + $json2')
+fi
+
+TRIGGERED_PIPELINE=$(
+  curl --request POST \
+    --url "https://circleci.com/api/v2/project/$PARAM_PROJECT/pipeline" \
+    --header "Circle-Token: $CIRCLE_TOKEN" \
+    --header "content-type: application/json" \
+    --data "{\"branch\":\"$PARAM_BRANCH\",\"parameters\": \"$PARAMETERS\"}" |
+    jq -r '.id'
+)
+
+echo "Triggered pipeline $TRIGGERED_PIPELINE"
+echo $TRIGGERED_PIPELINE >CIRCLEBACK_ORB_PIPELINE_ID
